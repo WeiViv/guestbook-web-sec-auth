4a07d5a8fb07cfca9305cbaeccc2a3390ea1560b
WeiViv <100058931+WeiViv@users.noreply.github.com>
2025-05-20 15:54:06 -0500

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,7 +4,7 @@ export const getDatabase = () => {
   const db = mysql.createConnection({
     multipleStatements: true,
     host     : '0.tcp.ngrok.io',
-    port     : 11916,
+    port     : 17377,
     user     : '396user',
     password : '396user', //Not secure to store it this way; fill in yourself
     database : 'guestbook'
